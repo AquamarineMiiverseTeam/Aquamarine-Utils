db48a16ca86a1648b65e7eb024002f3b61f7143d
NoNameGivenCoder <chaytontimedis@gmail.com>
2024-01-21 09:29:56 -0700

    added account_data middleware fully

--- common.js
+++ common.js
@@ -1,9 +1,11 @@
 const colors = require(process.cwd() + '/node_modules/colors');
 const moment = require(process.cwd() + '/node_modules/moment');
 
+const util = require('util')
 const con = require('./database_con');
 const query = util.promisify(con.query).bind(con);
 
+//This is a utility object to get certain things about an account, or create certain things.
 const utility = {
     notification : {
         createNewNotification : async function createNewNotification(account, from_account_id, type, image_url, content, content_id) {
@@ -15,11 +17,17 @@ const utility = {
         },
 
         getAccountUnreadNotifications : async function getAccountUnreadNotifications(account) {
-            return await query("SELECT FROM notifications WHERE account_id=? AND read_notif=0", account[0].id)
+            return await query("SELECT * FROM notifications WHERE account_id=? AND read_notif=0", account[0].id)
         },
 
         getAccountAllNotifications : async function getAccountAllNotifications(account) {
-            return await query("SELECT FROM notifications WHERE account_id=?", account[0].id)
+            return await query("SELECT * FROM notifications WHERE account_id=?", account[0].id)
+        }
+    },
+
+    empathy : {
+        getAccountEmpathiesGiven : async function getAccountEmpathiesGiven(account) {
+            return await query("SELECT * FROM empathies WHERE account_id=?", account[0].id)
         }
     }
 }
--- middleware/account_data_middleware.js
+++ middleware/account_data_middleware.js
@@ -1,7 +1,8 @@
 const colors = require(process.cwd() + '/node_modules/colors');
 const moment = require(process.cwd() + '/node_modules/moment');
 
-const con = require('./database_con');
+const con = require('../database_con');
+const util = require('util')
 const query = util.promisify(con.query).bind(con);
 
 const common = require("../common")
@@ -9,9 +10,15 @@ const common = require("../common")
 async function getAccountData(req, res, next) {
     //This middleware is meant for getting all extra account data that may be used in places such as
     //the navbar, and other UI elements
+    if (req.path.includes("img") || req.path.includes("css") || req.path.includes("js")) { next(); return; }
 
     req.account.all_notifications = await common.notification.getAccountAllNotifications(req.account);
     req.account.unread_notifications = await common.notification.getAccountUnreadNotifications(req.account);
+    req.account.empathies_given = await common.empathy.getAccountEmpathiesGiven(req.account);
+
+    console.log(req.account)
 
     next();
 }
+
+module.exports = getAccountData
